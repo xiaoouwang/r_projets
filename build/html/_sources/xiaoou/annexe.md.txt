61d96a3b740de207a0126c90ba466a166096d2ef
xiaoou <xiaoouwangfrance@gmail.com>
2021-03-08 15:58:19 +0100

    refresh

---
+++ build/html/_sources/xiaoou/annexe.md.txt
@@ -0,0 +1,3 @@
+# a markdown test
+
+![](img/annexe/2021-03-08-15-57-53.png)
--- source/conf.py
+++ source/conf.py
@@ -17,6 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
+from recommonmark.parser import CommonMarkParser
+
 project = 'R projets en lingustique informatique'
 copyright = '2021, Xiaoou Wang'
 author = 'Xiaoou Wang'
@@ -62,3 +64,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
