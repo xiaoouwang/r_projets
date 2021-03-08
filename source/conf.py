1b36ffa9a3ca09df6b5992d7fa96134a2b5d89c4
xiaoou <xiaoouwangfrance@gmail.com>
2021-03-08 17:06:18 +0100

    refresh

--- source/conf.py
+++ source/conf.py
@@ -57,7 +57,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+# html_theme = 'sphinx_rtd_theme'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
